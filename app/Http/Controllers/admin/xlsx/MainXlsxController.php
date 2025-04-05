263f22da6543a8d83847e8346f6b012b4cafe987
Your Name <you@example.com>
2025-04-05 11:08:53 +0300

    a

---
+++ app/Http/Controllers/admin/xlsx/MainXlsxController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\admin\xlsx;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MainXlsxController extends Controller
+{
+    //
+}
